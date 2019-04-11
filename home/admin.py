426bc4689de2943043585ec65ed5593eaa9f6b5e
Aidan Gallagher <ajgcpg1@gmail.com>
2019-04-11 16:01:37 -0400

    added app database

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from home.models import App
 
-# Register your models here.
+class HomeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(App, HomeAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class App(models.Model):
+    title = models.CharField(max_length=255)
+    blurb = models.TextField(max_length=255)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
