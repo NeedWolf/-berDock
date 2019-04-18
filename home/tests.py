e116f8f07dcb5bcf7fd6b165b06fdd3d154a2289
NeedWolf <cogansj2@gmail.com>
2019-04-18 19:37:12 -0400

    Added a unit case

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import App
 
-# Create your tests here.
+
+def create_app(app_title, app_blurb, app_url):
+    return App.objects.create(title=app_title, blurb=app_blurb)
+
+class AppTestCase(TestCase):
+    # Creating an app
+    # Test 1: Viewing the app
+    def testing_app_created(self):
+        future_app = create_app("Test", "Blurb1", "google.com")
+
+
+
+
+
