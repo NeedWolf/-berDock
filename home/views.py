0fd086256b458ddbff613f282a4d38c2141513fe
Aidan Gallagher <ajgcpg1@gmail.com>
2019-04-18 09:34:50 -0400

    searchbar functionality

--- home/views.py
+++ home/views.py
@@ -6,4 +6,15 @@ from django.http import HttpResponse
 
 class AppView(ListView):
     model = App
+    paginate_by = 50
     template_name = 'home/index.html'
+
+    def get_queryset(self):
+        filter_val = self.request.GET.get('search', '')
+        new_context = App.objects.filter(title__contains=filter_val)
+        return new_context
+
+    def get_context_data(self, **kwargs):
+        context = super(AppView, self).get_context_data(**kwargs)
+        context['search'] = self.request.GET.get('search', '')
+        return context
