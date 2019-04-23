545af8c3f7095ac25dbf2f14ea654b8ac1f2a5a1
Scott <47955536+NeedWolf@users.noreply.github.com>
2019-04-23 07:27:53 -0400

    Removed incorrect comment

--- home/tests.py
+++ home/tests.py
@@ -8,7 +8,6 @@ def create_app(app_title, app_blurb, app_url):
 
 class AppTestCase(TestCase):
     # Creating an app
-    # Test 1: Viewing the app
     def testing_app_created(self):
         future_app = create_app("Test", "Blurb1", "google.com")
 
