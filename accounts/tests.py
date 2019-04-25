def6734f913eb5d2d16908576254bad4b11fc58b
Scott <47955536+NeedWolf@users.noreply.github.com>
2019-04-25 19:44:30 -0400

    Added unitTests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestTests(TestCase):
+    def test1(self):
+        self.assertEqual(2,3)
+    def test2(self):
+        self.assertEqual(2,2)
+    def test3(self):
+        x = 5
+        self.assertEqual(x,6)
+    def test4(self):
+        x = 5
+        self.assertEqual(x,5)
