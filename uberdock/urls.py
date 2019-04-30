a39479c8ebb89d60983ca761627b39b48640996d
Aidan Gallagher <ajgcpg1@gmail.com>
2019-04-30 17:04:05 -0400

    testing

--- uberdock/urls.py
+++ uberdock/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls, name = "admin"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
 
 ]
