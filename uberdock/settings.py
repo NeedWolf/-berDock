1819966342997eb5e8e9947cfcaa4c3e9d2dad29
Aidan Gallagher <ajgcpg1@gmail.com>
2019-04-30 18:22:50 -0400

    reverting changes

--- uberdock/settings.py
+++ uberdock/settings.py
@@ -39,10 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_comments',
-    'django.contrib.sites',
 ]
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- uberdock/urls.py
+++ uberdock/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
+
 ]
