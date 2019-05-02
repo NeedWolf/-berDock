2a895ca27fbd325ef40111dbefd46a223dbc670b
Alex Heim <alexheim1998@gmail.com>
2019-05-01 21:10:15 -0400

    Added some stuff to test_urls.py to start off with (can't test from my computer apparently, so take w/ grain of salt)

--- home/tests/test_urls.py
+++ home/tests/test_urls.py
@@ -6,3 +6,15 @@ class TestUrls(SimpleTestCase):
         assert 1 == 2
         name = "AppName"
         self.assertEquals(resolve(name).func, AppView)
+
+        name = "AppSearch"
+        self.assertEquals(resolve(name).func, SearchView)
+
+        name = "AppDetails"
+        self.assertEquals(resolve(name).func, AppDetailView)
+
+        name = "AppUpdateString"
+        self.assertEquals(resolve(name).func, AppUpdateView)
+
+        name = "App Create View"
+        self.assertEquals(resolve(name).func, AppCreateView)
