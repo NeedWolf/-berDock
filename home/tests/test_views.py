5b4258003ec8cf72c2d156ac4358cbeb1ea308f9
Alex Heim <alexheim1998@gmail.com>
2019-05-02 17:44:33 -0400

    Added a tad to test_views.py

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -2,12 +2,17 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from home.models import App, AppRequest
 import json
-
 class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.list_url = reverse('list')
+        self.detail_url = reverse('detail')
+
     def test_project_get_SELF(self):
         client = Client()
 
-        response = client.get(reverse("index"))
+        response = client.get(reverse('index'))
 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "home/index.html")
+        self.assertTemplateUsed(response, 'home/index.html')
