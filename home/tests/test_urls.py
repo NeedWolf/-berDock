94a27797d02280d72007c2f5b6e45ca08015071c
Alex Heim <alexheim1998@gmail.com>
2019-05-01 21:05:18 -0400

    Added some stuff to test_urls.py to start off with (can't test from my computer apparently, so take w/ grain of salt)

--- home/tests/test_urls.py
+++ home/tests/test_urls.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
+from home.views import AppView, SearchView, AppDetailView, AppUpdateView, AppCreateView
 
 class TestUrls(SimpleTestCase):
     def test_url(self):
         assert 1 == 2
-        assert 1 == 1
+        name = "AppName"
+        self.assertEquals(resolve(name).func, AppView)
