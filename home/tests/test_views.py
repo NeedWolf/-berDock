822b746188f75a98afc7ca7c298af338e54d0efe
Alex Heim <alexheim1998@gmail.com>
2019-05-09 18:18:20 -0400

    (Tried to) Fix the setUp for the test_views class.

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -6,7 +6,7 @@ import json
 class TestViews(TestCase):
     # test designed to fail, first off.
     def setUp(self):
-        App.objects.create(self)
+        App.objects.create()
 
     def test_project_get_SELF(self):
         client = Client()
@@ -26,48 +26,48 @@ class TestViews(TestCase):
     def test_AppCreateView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(AppCreateView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_new.html')
 
     def test_AppDetailView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(AppDetailView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail.html')
 
     def test_AppUpdateView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(AppUpdateView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_search.html')
 
     def test_SearchView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(SearchView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail.html')
 
     def test_AllappCreateView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(AllappCreateView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'apost_new.html')
 
     def test_AllappDetailView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(AllappDetailView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'apost_detail.html')
 
     def test_AllappView(self):
         client = Client()
 
-        response = client.get(reverse(AppView.template_name))
+        response = client.get(reverse(AllappView.template_name))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'all_app.html')
