3f4da553dcd4b47a9ec2aaaef999fc3d3c0a2f10
Aidan Gallagher <ajgcpg1@gmail.com>
2019-05-09 14:38:14 -0400

    styles

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -8,8 +8,8 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
 
-        self.list_url = reverse('list')
-        self.detail_url = reverse('detail')
+        # self.list_url = reverse('list')
+        # self.detail_url = reverse('detail')
 
     def test_project_get_SELF(self):
         client = Client()
