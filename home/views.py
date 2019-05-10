e9af5b90e7f6022a50577e07ef14d95ad04742a2
Aidan Gallagher <ajgcpg1@gmail.com>
2019-05-10 19:53:00 -0400

    insensitive search

--- home/views.py
+++ home/views.py
@@ -33,7 +33,7 @@ class SearchView(ListView):
 
     def get_queryset(self):
         filter_val = self.request.GET.get('search', '')
-        new_context = App.objects.filter(title__contains=filter_val)
+        new_context = App.objects.filter(title__icontains=filter_val)
         return new_context
 
     def get_context_data(self, **kwargs):
