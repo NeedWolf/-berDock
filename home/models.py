648bbb09349c70c74ed018e09b13e521cdd47edb
Aidan Gallagher <ajgcpg1@gmail.com>
2019-05-10 20:49:39 -0400

    styles

--- home/models.py
+++ home/models.py
@@ -37,7 +37,7 @@ class App(models.Model):
 class AppRequest(models.Model):
     title = models.CharField(max_length=255)
     blurb = models.TextField(max_length=255)
-    link = models.URLField(default="http://uberdock.herokuapp.coms")
+    link = models.URLField(null=True)
 
     def __str__(self):
         return self.title
